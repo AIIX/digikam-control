267f9939a4fb310db041d12816e7cda724675f42
Aditya Mehra <Aix.m@outlook.com>
2018-03-27 20:17:56 +1030

    Initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# digikam-control
-This skill enables an user to control DigiKam on the Desktop.
+## Digikam-Control
+This skill enables an user to control the [DigiKam client](https://www.digikam.org/) on the Desktop.
+
+## Description 
+#### Installation of skill:
+* Download or Clone Git (run: git clone https://github.com/AIIX/DigiKam-control inside /opt/mycroft/skills)
+* Create /opt/mycroft/skills folder if it does not exist
+* Extract Downloaded Skill into a folder. "DigiKam-control". (Clone does not require this step)
+* Copy the DigiKam-control folder to /opt/mycroft/skills/ folder
+
+#### Installation of requirements:
+##### Fedora: 
+- sudo dnf install dbus-python
+- From terminal: cp -R /usr/lib64/python2.7/site-packages/dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
+- From terminal: cp /usr/lib64/python2.7/site-packages/_dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
+
+##### Ubuntu / KDE Neon: 
+- sudo apt install python-dbus
+- From terminal: cp -R /usr/lib/python2.7/dist-packages/dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
+- From terminal: cp /usr/lib/python2.7/dist-packages/_dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
+
+* For other distributions:
+- Python Dbus package is required and copying the Python Dbus folder and lib from your system python install over to /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/.
+
+## Examples
+##### Some of the actions, for actions refer to "actions.json" file: 
+
+###### Next Image
+* "Hey Mycroft, in digicam next image"
+
+###### Previous Image
+* "Hey Mycroft, in digicam previous image"
+
+###### Rotate Image Clockwise
+* "Hey Mycroft, in digicam rotate clockwise"
+
+###### Rotate Image Counter Clockwise
+* "Hey Mycroft, in digicam rotate counter clockwise"
+
+###### Flip-Horizontally
+* "Hey Mycroft, in digicam flip horizontally"
+
+###### Flip-vertically
+* "Hey Mycroft, in digicam flip vertically"
+
+
+## Credits 
+(AIX) Aditya Mehra
---
+++ __init__.py
@@ -0,0 +1,75 @@
+import re
+import json
+import dbus
+from adapt.intent import IntentBuilder
+from os.path import join, dirname
+from string import Template
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.skills.context import *
+from mycroft.util import read_stripped_lines
+from mycroft.util.log import getLogger
+from mycroft.messagebus.message import Message
+
+__author__ = 'aix'
+
+LOGGER = getLogger(__name__)
+
+class DigiKamControlSkill(MycroftSkill):
+    def __init__(self):
+        super(DigiKamControlSkill, self).__init__(name="DigiKamControlSkill")
+        self.actions_index = dirname(__file__) + '/actions.json'
+        
+    @intent_handler(IntentBuilder("DigiKamControl").require("DigiCamKeyword").build())
+    def handle_digikam_control_intent(self, message):
+        utterance = message.data.get('utterance').lower()
+        utterance = utterance.replace(message.data.get('DigiCamKeyword'), '')
+        searchString = utterance.encode('utf-8')
+        getAction = self.filterAction(searchString.lstrip())
+       
+        session_bus = dbus.SessionBus()
+        digicam_bus = ""
+        for method in session_bus.list_names():
+            if method.find("digikam") != -1:
+                print method
+                digicam_bus = method
+                break
+
+        digicamObj = session_bus.get_object(digicam_bus, "/Digikam")
+        digicamObj.activateAction(getAction, dbus_interface = "org.kde.KMainWindow")
+       
+    def filterAction(self, event):
+        action = event.lower()
+        with open(self.actions_index) as json_data:
+            d = json.load(json_data)
+            if action in d["next_image"]:
+                return "next_image"
+            elif action in d["previous_image"]:
+                return "previous_image"
+            elif action in d["first_image"]:
+                return "first_image"
+            elif action in d["last_image"]:
+                return "last_image"
+            elif action in d["view_refresh"]:
+                return "view_refresh"
+            elif action in d["browse_album"]:
+                return "browse_album"
+            elif action in d["rotate_ccw"]:
+                return "rotate_ccw"
+            elif action in d["rotate_cw"]:
+                return "rotate_cw"
+            elif action in d["flip_horizontal"]:
+                return "flip_horizontal"
+            elif action in d["flip_vertical"]:
+                return "flip_vertical"
+            elif action in d["selectAll"]:
+                return "selectAll"
+            elif action in d["selectInvert"]:
+                return "selectInvert"
+            else:
+                self.speak("Action Not Found");
+        
+    def stop(self):
+        pass
+    
+def create_skill():
+    return DigiKamControlSkill()
